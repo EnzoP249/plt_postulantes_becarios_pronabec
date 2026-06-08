67d06e0a915964b9acb3ed90bde25ecc35f265f1
EnzoPeña <enzo.lpge@gmail.com>
2026-06-08 12:47:38 -0500

    Se agrega análisis geoespacial

--- plt_postulante_pronabec.py
+++ plt_postulante_pronabec.py
@@ -21,6 +21,9 @@ import seaborn as sns
 from matplotlib.ticker import FuncFormatter
 from rapidfuzz import process, fuzz
 from unidecode import unidecode
+import geopandas
+from shapely.geometry import Point, Polygon
+from matplotlib.colors import LinearSegmentedColormap
 
 ###############################################################################
 # Se describen los colores que integran la paleta institucional para mis gráficos
@@ -53,7 +56,7 @@ def int_to_str(value):
 # Especifica el diccionario de conversión en el parámetro converters
 converters = {"ID_POSTULACION": int_to_str}
 
-pronabec = pd.read_excel("0_BGB_2013_2025.xlsx", sheet_name="Sheet1", header=0, converters=converters)
+pronabec = pd.read_excel("BGB_2013_2025_updt.xlsx", sheet_name="Sheet1", header=0, converters=converters)
 
 # Se identifican caracteristicas estructurales del dataframe pronabec
 pronabec.shape
@@ -62,9 +65,8 @@ pronabec.dtypes
 pronabec.columns
 
 
-
 # Se reorganiza la información que integra el objeto dataframe pronabec
-pronabec = pronabec[["N","ID_POSTULACION", "CONDICION_FINAL", "NEXPEDIENTE", "AÑO_CONVOCATORIA",
+pronabec = pronabec[["N","ID_POSTULACION", "CONDICION_FINAL", "NEXPEDIENTE", "AÑO_CONVOCATORIA","SEXO", "EDADBASES",
                      "PAISDESTINO", "NIVEL_EDUCATIVO","INSTITUCION", "TIPO_GESTION", "NOMBRECARRERA", "AREA_CONOCIMIENTO",
                             "FECHA_INICIO_PROGRAMA", "FECHA_FIN_PROGRAMA", "DURACION_DIAS",
                             'COMPROMISO_DE_SERVICIO_AL_PAIS', 'ESTADO_DE_CSP',
@@ -105,6 +107,13 @@ postulante = postulante[postulante["NIVEL_EDUCATIVO"]!="NO CODIFICADA"]
 
 # Se identifica si existen valores duplicados en el dataframe postulante
 postulante["ID_POSTULACION"].nunique()
+
+# Se analiza la distribución de postulantes por genero
+postulante.SEXO.value_counts(normalize=True).round(2)*100
+
+promedio_edad = postulante.groupby("SEXO")["EDADBASES"].mean()
+print(promedio_edad)
+
 
 # Se calcula la distribución de postulantes por año
 postulante_año = pd.pivot_table(postulante, values="ID_POSTULACION", index="AÑO_CONVOCATORIA", aggfunc="count")
@@ -317,10 +326,18 @@ print(f"Los postulantes que recibieron la beca, durante el periodo de análisis,
 pronabec_becario["ID_POSTULACION"].nunique()
 pronabec_becario.columns
 
+# Se analiza la distribución de postulantes por genero
+pronabec_becario.SEXO.value_counts(normalize=True).round(2)*100
+
+promedio_edad = pronabec_becario.groupby("SEXO")["EDADBASES"].mean()
+print(promedio_edad)
+
+
+# Se calcula la distribución de las categorias que integran la columna ESTADO_DE_CSP
+pronabec_becario.ESTADO_DE_CSP.value_counts()
+
 # Renombro valores de la columna ESTADO_DE_CSP
 pronabec_becario["ESTADO_DE_CSP"] = pronabec_becario["ESTADO_DE_CSP"].fillna("SIN INFORMACIÓN")
-
-
 
 ###############################################################################
 # Considerando el dataframe pronabec_becario, se obtiene la evolución de los becarios, a nivel temporal
@@ -1033,7 +1050,7 @@ wedges, texts, autotexts = ax.pie(
 # Estilo de porcentajes
 for autotext in autotexts:
     autotext.set_color("white")
-    autotext.set_fontsize(13)
+    autotext.set_fontsize(14)
     autotext.set_fontweight("bold")
 
 # Título
@@ -1091,6 +1108,200 @@ wedges, texts, autotexts = ax.pie(
 # Estilo de porcentajes
 for autotext in autotexts:
     autotext.set_color("white")
+    autotext.set_fontsize(14)
+    autotext.set_fontweight("bold")
+
+# Título
+#ax.set_title(
+    #"Distribución de programas STEM y No STEM",
+    #fontsize=15,
+    #fontweight="bold",
+    #pad=20
+#)
+
+# Mantener círculo perfecto
+ax.axis("equal")
+
+plt.tight_layout()
+plt.show()
+
+###############################################################################
+# Se analiza el estado de los becarios de maestria y doctorado del 2022 al 2025
+###############################################################################
+
+segunda = pronabec_becario[(pronabec_becario["AÑO_CONVOCATORIA"] >= 2022) & (pronabec_becario["AÑO_CONVOCATORIA"] <= 2025)]
+segunda.columns
+
+# Se renombran algunos registros de la columna INSTITUCION_ORIGEN_PREGRADO del dataframe segunda
+segunda["INSTITUCION_ORIGEN_PREGRADO"] = segunda["INSTITUCION_ORIGEN_PREGRADO"].replace({
+    "PONTIFICIA UNIVERSIDAD CATOLICA DEL PERU":
+    "PONTIFICIA UNIVERSIDAD CATÓLICA DEL PERÚ"
+})
+
+
+# Se renombran algunos registros de la columna REGION_PREGRADO del dataframe segunda
+segunda.REGION_PREGRADO.value_counts()
+segunda["REGION_PREGRADO"] = segunda["REGION_PREGRADO"].replace({
+    "HUANCAYO":"JUNIN",
+    "TRUJILLO":"LA LIBERTAD",
+    "HUACHO":"LIMA",
+    "CHACHAPOYAS":"AMAZONAS",
+    "CHOTA":"CAJAMARCA",
+    "TARAPOTO":"SAN MARTIN",
+    "TINGO MARIA ":"HUANUCO",
+    "JAÉN-CAJAMARCA":"CAJAMARCA",
+    "IQUITOS":"LORETO",
+    "CALLAO":"LIMA"
+    })
+   
+    
+# Se analiza la formación académica de los becarios de maestria
+segunda_maestria = segunda[segunda["NIVEL_EDUCATIVO"]=="MAESTRIA"]
+segunda_maestria_ima = segunda_maestria.GRADO_PREGRADO.value_counts(normalize=True).round(3)*100
+segunda_maestria_ima = segunda_maestria_ima.to_frame()
+segunda_maestria_ima.reset_index(inplace=True)
+segunda_maestria_ima.rename(columns=({"proportion":"porcentaje"}), inplace=True)
+segunda_maestria_ima["porcentaje"] = segunda_maestria_ima["porcentaje"].astype("float")
+
+fig, ax = plt.subplots(figsize=(10, 6))
+
+barras = ax.bar(
+    segunda_maestria_ima["GRADO_PREGRADO"],
+    segunda_maestria_ima["porcentaje"],
+    color="#2F6690",
+    edgecolor="white",
+    linewidth=1.5
+)
+
+# Etiquetas arriba de cada columna
+for barra in barras:
+    alto = barra.get_height()
+    ax.text(
+        barra.get_x() + barra.get_width() / 2,
+        alto,
+        f"{alto:,.1f}",
+        ha="center",
+        va="bottom",
+        fontsize=14,
+        #fontweight="bold",
+        color="black"
+    )
+
+
+#ax.set_title(
+    #"Evolución del número total de postulantes, 2013-2025",
+    #fontsize=14,
+    #fontweight="bold"
+#)
+
+plt.xlabel("Grado académico")
+plt.ylabel("Porcentaje (%)")
+
+
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+
+plt.tight_layout()
+plt.show()
+
+
+# Se analiza la formación académica de los becarios de doctorado
+
+segunda_doctorado = segunda[segunda["NIVEL_EDUCATIVO"]=="DOCTORADO"]
+segunda_doctorado_ima = (
+    segunda_doctorado["GRADO_PREGRADO"]
+    .value_counts(normalize=True)
+    .mul(100)
+    .to_frame(name="porcentaje")
+    .reset_index()
+)
+
+
+
+fig, ax = plt.subplots(figsize=(10, 6))
+
+barras = ax.bar(
+    segunda_doctorado_ima["GRADO_PREGRADO"],
+    segunda_doctorado_ima["porcentaje"],
+    color="#2F6690",
+    edgecolor="white",
+    linewidth=1.5
+)
+
+# Etiquetas arriba de cada columna
+for barra in barras:
+    alto = barra.get_height()
+    ax.text(
+        barra.get_x() + barra.get_width() / 2,
+        alto,
+        f"{alto:,.3f}",
+        ha="center",
+        va="bottom",
+        fontsize=14,
+        #fontweight="bold",
+        color="black"
+    )
+
+
+#ax.set_title(
+    #"Evolución del número total de postulantes, 2013-2025",
+    #fontsize=14,
+    #fontweight="bold"
+#)
+
+plt.xlabel("Grado académico")
+plt.ylabel("Porcentaje (%)")
+
+
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+
+plt.tight_layout()
+plt.show()
+
+###############################################################################
+# Se analiza el tipo de gestión de la universdad del pregrado de becarios tanto
+# de programas de maestria como doctorado
+###############################################################################
+
+# Becarios de programas de maestria
+
+gestion_maestria = segunda_maestria.GESTION_PREGRADO.value_counts(normalize=True).round(2)*100
+gestion_maestria = gestion_maestria.to_frame()
+gestion_maestria.reset_index(inplace=True)
+gestion_maestria.rename(columns=({"proportion":"porcentaje"}), inplace=True)
+gestion_maestria.columns
+
+
+labels = gestion_maestria["GESTION_PREGRADO"]
+sizes = gestion_maestria["porcentaje"]
+
+# Colores institucionales
+colors = ["#264653", "#8FAF3C"]
+
+# Crear figura
+fig, ax = plt.subplots(figsize=(8, 6))
+
+wedges, texts, autotexts = ax.pie(
+    sizes,
+    labels=labels,
+    colors=colors,
+    autopct="%1.0f%%",
+    startangle=90,
+    counterclock=False,
+    wedgeprops={
+        "edgecolor": "white",
+        "linewidth": 2
+    },
+    textprops={
+        "fontsize": 12,
+        "fontweight": "bold"
+    }
+)
+
+# Estilo de porcentajes
+for autotext in autotexts:
+    autotext.set_color("white")
     autotext.set_fontsize(13)
     autotext.set_fontweight("bold")
 
@@ -1109,8 +1320,294 @@ plt.tight_layout()
 plt.show()
 
 
+# Becarios de programas de doctorado
+
+gestion_doctorado = segunda_doctorado.GESTION_PREGRADO.value_counts(normalize=True).round(2)*100
+gestion_doctorado = gestion_doctorado.to_frame()
+gestion_doctorado.reset_index(inplace=True)
+gestion_doctorado.rename(columns=({"proportion":"porcentaje"}), inplace=True)
+gestion_doctorado.columns
 
 
+labels = gestion_doctorado["GESTION_PREGRADO"]
+sizes = gestion_doctorado["porcentaje"]
 
+# Colores institucionales
+colors = ["#264653", "#8FAF3C"]
+
+# Crear figura
+fig, ax = plt.subplots(figsize=(8, 6))
+
+wedges, texts, autotexts = ax.pie(
+    sizes,
+    labels=labels,
+    colors=colors,
+    autopct="%1.0f%%",
+    startangle=90,
+    counterclock=False,
+    wedgeprops={
+        "edgecolor": "white",
+        "linewidth": 2
+    },
+    textprops={
+        "fontsize": 12,
+        "fontweight": "bold"
+    }
+)
+
+# Estilo de porcentajes
+for autotext in autotexts:
+    autotext.set_color("white")
+    autotext.set_fontsize(13)
+    autotext.set_fontweight("bold")
+
+# Título
+#ax.set_title(
+    #"Distribución de programas STEM y No STEM",
+    #fontsize=15,
+    #fontweight="bold",
+    #pad=20
+#)
+
+# Mantener círculo perfecto
+ax.axis("equal")
+
+plt.tight_layout()
+plt.show()
+
+###############################################################################
+# Se analiza la universidad de procedencia (Top 10) de los becarios de programa
+# de maestria y doctorado
+###############################################################################
+
+# Se realiza el análisis para becarios de programas de maestría
+
+pais_segunda_maestria = segunda_maestria.INSTITUCION_ORIGEN_PREGRADO.value_counts(normalize=True).round(2)*100
+pais_segunda_maestria = pais_segunda_maestria.to_frame()
+pais_segunda_maestria.reset_index(inplace=True)
+pais_segunda_maestria.rename(columns=({"proportion":"Porcentaje (%)"}), inplace=True)
+pais_segunda_maestria = pais_segunda_maestria.head(10)
+pais_segunda_maestria.columns
+
+pais_segunda_maestria = pais_segunda_maestria.sort_values("Porcentaje (%)", ascending=True)
+
+color_principal = "#0B4F6C"   # azul institucional
+color_secundario = "#5FB7C6"  # celeste
+
+colors = [color_secundario] * len(pais_maestria)
+colors[-1] = color_principal  # destacar el mayor
+
+plt.figure(figsize=(12, 6))
+
+bars = plt.barh(pais_segunda_maestria["INSTITUCION_ORIGEN_PREGRADO"], pais_segunda_maestria["Porcentaje (%)"], color=colors)
+
+# Etiquetas
+for i, v in enumerate(pais_segunda_maestria["Porcentaje (%)"]):
+    plt.text(v + 0.5, i, f"{v:.0f}%", va="center", fontsize=10)
+
+# Estética consultoría
+plt.xlabel("Porcentaje (%)")
+#plt.title("Distribución de subvenciones por tipo de intervención", fontsize=13)
+
+plt.gca().spines["top"].set_visible(False)
+plt.gca().spines["right"].set_visible(False)
+
+plt.grid(axis="x", linestyle="--", alpha=0.3)
+
+plt.tight_layout()
+plt.show()
+
+
+# Se realiza el análisis para becarios de programas de doctorado
+
+pais_segunda_doctorado = segunda_doctorado.INSTITUCION_ORIGEN_PREGRADO.value_counts(normalize=True).round(2)*100
+pais_segunda_doctorado = pais_segunda_doctorado.to_frame()
+pais_segunda_doctorado.reset_index(inplace=True)
+pais_segunda_doctorado.rename(columns=({"proportion":"Porcentaje (%)"}), inplace=True)
+pais_segunda_doctorado = pais_segunda_doctorado.head(10)
+pais_segunda_doctorado.columns
+
+pais_segunda_doctorado = pais_segunda_doctorado.sort_values("Porcentaje (%)", ascending=True)
+
+color_principal = "#0B4F6C"   # azul institucional
+color_secundario = "#5FB7C6"  # celeste
+
+colors = [color_secundario] * len(pais_segunda_doctorado)
+colors[-1] = color_principal  # destacar el mayor
+
+plt.figure(figsize=(12, 6))
+
+bars = plt.barh(pais_segunda_doctorado["INSTITUCION_ORIGEN_PREGRADO"], pais_segunda_doctorado["Porcentaje (%)"], color=colors)
+
+# Etiquetas
+for i, v in enumerate(pais_segunda_doctorado["Porcentaje (%)"]):
+    plt.text(v + 0.5, i, f"{v:.0f}%", va="center", fontsize=10)
+
+# Estética consultoría
+plt.xlabel("Porcentaje (%)")
+#plt.title("Distribución de subvenciones por tipo de intervención", fontsize=13)
+
+plt.gca().spines["top"].set_visible(False)
+plt.gca().spines["right"].set_visible(False)
+
+plt.grid(axis="x", linestyle="--", alpha=0.3)
+
+plt.tight_layout()
+plt.show()
+
+###############################################################################
+# Se analiza las regiones de los becarios de programas de maestria y doctorado
+###############################################################################
+
+# Becarios de programas de maestria
+
+peru_maestria = segunda_maestria.REGION_PREGRADO.value_counts()
+peru_maestria = peru_maestria.to_frame()
+peru_maestria.reset_index(inplace=True)
+peru_maestria.rename(columns=({"REGION_PREGRADO":"Region", "count":"cantidad"}), inplace=True)
+
+
+#código para la elaboración de un mapa de calor, considerando una variable específica, utilizando geopandas
+# Se construye el mapa de Perú, a nivel departamental
+peru = geopandas.read_file("INEI_LIMITE_DEPARTAMENTAL_GEOGPSPERU_JUANSUYO_931381206.shp")
+peru.columns
+peru.rename(columns=({"NOMBDEP":"Region"}), inplace=True)
+
+# Se fusiona el dataframe peru con region
+peru = pd.merge(peru,peru_maestria, on="Region", how="left")
+
+
+#Paleta institucional
+custom_cmap = LinearSegmentedColormap.from_list(
+    "prociencia_palette",
+    [
+        "#E6F4F7",  # celeste muy claro
+        "#00A7B5",  # turquesa
+        "#1F6F8B"   # azul petróleo
+    ]
+)
+
+fig, ax = plt.subplots(figsize=(10, 6))
+
+ax.axis("off")
+
+# Mapa base (regiones sin datos en gris)
+peru.plot(
+    ax=ax,
+    color="#D9D9D9",
+    edgecolor="white",
+    linewidth=0.8
+)
+
+# Regiones con datos
+peru.plot(
+    column="cantidad",
+    cmap=custom_cmap,
+    linewidth=0.8,
+    ax=ax,
+    edgecolor="white",
+    legend=True
+)
+
+# Etiquetas
+for idx, row in peru.iterrows():
+
+    if pd.notna(row["cantidad"]):
+
+        # Coordenadas del centroide
+        x = row.geometry.centroid.x
+        y = row.geometry.centroid.y
+
+        # Texto blanco para regiones oscuras
+        color_texto = "white" if row["cantidad"] >= 20 else "black"
+
+        ax.annotate(
+            text=f"{int(row['cantidad'])}",
+            xy=(x, y),
+            ha="center",
+            va="center",
+            fontsize=9,
+            fontweight="bold",
+            color=color_texto
+        )
+
+plt.tight_layout()
+plt.show()
+
+
+# Becarios de programas de doctorado
+
+peru_doctorado = segunda_doctorado.REGION_PREGRADO.value_counts()
+peru_doctorado = peru_doctorado.to_frame()
+peru_doctorado.reset_index(inplace=True)
+peru_doctorado.rename(columns=({"REGION_PREGRADO":"Region", "count":"cantidad"}), inplace=True)
+
+
+#código para la elaboración de un mapa de calor, considerando una variable específica, utilizando geopandas
+# Se construye el mapa de Perú, a nivel departamental
+peru = geopandas.read_file("INEI_LIMITE_DEPARTAMENTAL_GEOGPSPERU_JUANSUYO_931381206.shp")
+peru.columns
+peru.rename(columns=({"NOMBDEP":"Region"}), inplace=True)
+
+# Se fusiona el dataframe peru con region
+peru = pd.merge(peru,peru_doctorado, on="Region", how="left")
+
+
+#Paleta institucional
+custom_cmap = LinearSegmentedColormap.from_list(
+    "prociencia_palette",
+    [
+        "#E6F4F7",  # celeste muy claro
+        "#00A7B5",  # turquesa
+        "#1F6F8B"   # azul petróleo
+    ]
+)
+
+fig, ax = plt.subplots(figsize=(10, 6))
+
+ax.axis("off")
+
+# Mapa base (regiones sin datos en gris)
+peru.plot(
+    ax=ax,
+    color="#D9D9D9",
+    edgecolor="white",
+    linewidth=0.8
+)
+
+# Regiones con datos
+peru.plot(
+    column="cantidad",
+    cmap=custom_cmap,
+    linewidth=0.8,
+    ax=ax,
+    edgecolor="white",
+    legend=True
+)
+
+# Etiquetas
+for idx, row in peru.iterrows():
+
+    if pd.notna(row["cantidad"]):
+
+        # Coordenadas del centroide
+        x = row.geometry.centroid.x
+        y = row.geometry.centroid.y
+
+        # Texto blanco para regiones oscuras
+        color_texto = "white" if row["cantidad"] >= 14 else "black"
+
+        ax.annotate(
+            text=f"{int(row['cantidad'])}",
+            xy=(x, y),
+            ha="center",
+            va="center",
+            fontsize=9,
+            fontweight="bold",
+            color=color_texto
+        )
+
+plt.tight_layout()
+plt.show()
 
 
